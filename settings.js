b95964edc42ba7e80a658f7fb2058357f0cf042d
Kirito596 <120283034+Kirito596@users.noreply.github.com>
2023-02-16 18:59:46 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,7 +14,7 @@ global.DocumentTitle = "꧁ঔৣ༒✞𝕿𝖆𝖐𝖎𝖓𝖆/𝕮𝖍𝖎𝖘
 
 global.ownername = "꧁ঔৣ༒✞𝕿𝖆𝖐𝖎𝖓𝖆/𝕮𝖍𝖎𝖘𝖆𝖙𝖔✞༒ঔৣ꧂"  // Owner name //        
 global.ownernumber = ['491777062822'] 
-global.ownername = "Zyxo-1119"
+global.ownername = "Zyxo-1119"  // Owner name //
 global.ownernumber = ['+49 179 2911493']
 global.socialm = "𝗜𝗻𝘀𝘁𝗮 : ꧁ঔৣ༒✞𝕿𝖆𝖐𝖎𝖓𝖆/𝕮𝖍𝖎𝖘𝖆𝖙𝖔✞༒ঔৣ꧂" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
 global.ytname = "YT: Ytname"   // Yt chanel name //
